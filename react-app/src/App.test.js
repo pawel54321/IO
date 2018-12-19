7c28f70990577a537e1f75c2dfc453b5256f20c9
gpzak <grzegorz.p.zak@gmail.com>
2018-12-19 18:20:57 +0100

    Update App.test.js

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -1,5 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Header from './Components/Header';
+import DashboardAdmin from './Components/DashboardAdmin';
+import DashboardUser from './Components/DashboardUser';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import Footer from './Components/Footer';
+import HomePage from './Components/HomePage';
 
-it('renders without crashing', () => {});
+
+it('renders without crashing', () => {
+  shallow(<App />);
+});
+
+it('renders without crashing', () => {
+  shallow(<Header />);
+});
+
+it('renders without crashing', () => {
+  shallow(<Footer />);
+});
+
+it('renders without crashing', () => {
+  shallow(<DashboardAdmin />);
+});
+
+it('renders without crashing', () => {
+  shallow(<DashboardUser />);
+});
+
+it('renders without crashing', () => {
+  shallow(<Login />);
+});
+
+it('renders without crashing', () => {
+  shallow(<Register />);
+});
+
+it('renders without crashing', () => {
+  shallow(<HomePage />);
+});
+
+it('includes Header', () => {
+  const app = shallow(<App />);
+  expect(app.containsMathingElement(<Header />)).toEqual(true)
+});
+
+it('includes Footer', () => {
+  const app = shallow(<App />);
+  expect(app.containsMathingElement(<Footer />)).toEqual(true)
+});
+
+it('includes HomePage', () => {
+  const app = shallow(<App />);
+  expect(app.containsMathingElement(<HomePage />)).toEqual(true)
+});
+
+it('includes DashboardAdmin', () => {
+  const app = shallow(<App />);
+  expect(app.containsMathingElement(<DashboardAdmin />)).toEqual(true)
+});
+
+it('includes DashboardUser', () => {
+  const app = shallow(<App />);
+  expect(app.containsMathingElement(<DashboardUser />)).toEqual(true)
+});
+
+it('includes Login', () => {
+  const app = shallow(<App />);
+  expect(app.containsMathingElement(<Login />)).toEqual(true)
+});
+
+it('includes Register', () => {
+  const app = shallow(<App />);
+  expect(app.containsMathingElement(<Register />)).toEqual(true)
+});
