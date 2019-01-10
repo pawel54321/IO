ce1faae528fa4502a801086327cfd926c48de085
gpzak <grzegorz.p.zak@gmail.com>
2019-01-10 02:51:16 +0100

    Update App.test.js

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -26,28 +26,38 @@ configure({ adapter: new Adapter() });
 
 it('renders Header', () => {
   const header = shallow(<Header/>);
+  expect(header.containsMatchingElement('header')).toEqual(true);
+  expect(header.containsMatchingElement('Navbar')).toEqual(true);
+  expect(header.containsMatchingElement('NavbarBrand')).toEqual(true);
+  expect(header.containsMatchingElement('NavItem')).toEqual(true);
+  expect(header.containsMatchingElement('NavLink')).toEqual(true);
 });
 
 it('renders Footer', () => {
   const footer = shallow(<Footer/>);
-  expect(footer.contains('footer')).toEqual(true);
-  expect(footer.contains('Navbar')).toEqual(true);
-  expect(footer.contains('NavbarBrand')).toEqual(true);
-  expect(footer.contains('Link')).toEqual(true);
-  expect(footer.contains('SprawdzFooter')).toEqual(true);
+  expect(footer.containsMatchingElement('footer')).toEqual(true);
+  expect(footer.containsMatchingElement('Navbar')).toEqual(true);
+  expect(footer.containsMatchingElement('NavbarBrand')).toEqual(true);
+  expect(footer.containsMatchingElement('Link')).toEqual(true);
 });
 
 it('renders DashboardAdmin', () => {
   const da = shallow(<DashboardAdmin/>);
-  //expect(da.find(''))
+  expect(da.find('h1').text()).toEqual('Znajdujesz się w Panelu Administratora!');
 });
 
 it('renders DashboardUser', () => {
-  shallow(<DashboardUser/>);
+  const du = shallow(<DashboardUser/>);
+  expect(du.find('h5').text()).toEqual('Panel Użytkownika');
+  expect(du.find('p').text()).toEqual('Witaj! ');
 });
 
 it('renders Login', () => {
-  shallow(<Login/>);
+  const login = shallow(<Login/>);
+  expect(login.find('h5').text()).toEqual('Logowanie:'');
+  expect(login.find('Button').text()).toEqual('Zaloguj się!');
+  expect(login.find('p').text()).toEqual('Nie masz konta? ');
+  expect(login.find('Link').text()).toEqual('Zarejestruj się!');
 });
 
 it('renders Register', () => {
